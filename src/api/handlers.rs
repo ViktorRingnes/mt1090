aa9c0f6ceb4288c8c9bb2c5716ace52d1e4023e3
Viktor Ringnes <viktor_ringnes@icloud.com>
2026-06-29 17:14:57 +0200

    *

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -6,11 +6,9 @@ pub async fn lagre_fil_b2b_handler() -> impl IntoResponse {
     StatusCode::OK
 }
 
-
 pub async fn hent_unc_for_filer_handler() -> impl IntoResponse {
     StatusCode::OK
 }
-
 
 pub async fn ping_handler() -> impl IntoResponse {
     Json(Response {
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,6 @@
 pub mod handlers;
 pub mod dto;
 
-pub use handlers::{lagre_fil_b2b_handler, ping_handler, hent_unc_for_filer_handler};
-pub use dto::Response;
+pub use handlers::*;
+pub use dto::*;
 

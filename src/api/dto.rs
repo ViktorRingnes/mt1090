240ec430805a2305e1752c34f87cbec830cc9245
Viktor Ringnes <viktor_ringnes@icloud.com>
2026-06-30 14:38:08 +0200

    base64 fil input

--- src/api/dto.rs
+++ src/api/dto.rs
@@ -1,11 +1,18 @@
-use serde::{Deserialize, Serialize};
+use base64::{Engine, engine::general_purpose::STANDARD};
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct LagreFilB2BParameter {
+    #[serde(deserialize_with = "base64_bytes")]
     pub fil: Vec<u8>,
     pub filnavn: String,
     pub avsenderkode: String,
+}
+
+fn base64_bytes<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Vec<u8>, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    STANDARD.decode(s).map_err(serde::de::Error::custom)
 }
 
 #[derive(Debug, Serialize)]
